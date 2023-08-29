0ef33eebb1de16941bc31d9dae3d9aa7e7ff9e4d
Trevor Hilz <trevorhilz@gmail.com>
2023-08-29 16:26:47 -0500

    More code, rough version on grid

--- script.js
+++ script.js
@@ -1,26 +1,60 @@
 const container = document.getElementById('container');
 
-// function makeRowsSmall(rows, cols) {
-//     container.style.setProperty('--grid-rows', rows);
-//     container.style.setProperty('--grid-cols', cols); 
-//     for (let c = 0; c < (rows * cols); c++) {
-//         let cell = document.createElement("div");
-//         cell.innerText = (c + 1);
-//         container.appendChild(cell).className = "grid-item";
-//     };
-// };
-
-// const black = document.querySelector('black');
-// black.addEventListener('mouseover', () => {
-//     document.grid-item.body.style.backgroundColor; '#000000';
-// })
-
-// makeRowsSmall(16, 16);
-
-
-function createGrid (gridCount) {
-
+function makeGrid() {
+  for(let i = 0; i < 16; i++) {
+    let row = document.createElement('grid-item');
+    for(let j = 0; j < 16; j++) {
+      let cell = document.createElement('grid-item');
+      cell.style.width = "25px";
+      cell.style.height = "25px";
+      cell.style.background = "yellow";
+      cell.style.display = "inline-block";
+      cell.innerText = (".");
+      // cell.style.border = ".5px solid #000000";
+      row.appendChild(cell);
+      hoverColor();
+    }
+    document.getElementById('container').appendChild(row);
+  }
 }
 
-const gridInput = document.querySelectorAll('button');
-gridCount = gridCount;
+function theGrid () {
+  grid-template-column = 
+}
+
+makeGrid();
+
+function hoverColor (){
+  let items = document.querySelectorAll('grid-item');
+  items.forEach (item => {
+    item.addEventListener ('mouseover', () => {
+      item.style.backgroundColor = 'black';
+    })
+  })
+}
+
+sizeSelected();
+
+
+function sizeSelected () {
+  buttons = document.querySelectorAll('button');
+  let size;
+    buttons.forEach (button => {
+      button.addEventListener ('click', () => {
+        if (small) {
+          size = 16;
+        } else if (medium) {
+          size = 32;
+        } else if (large) {
+          size = 64;
+          
+        }
+        console.log(size);
+      })
+    })
+
+//   let sizeSmall = documentSelector('small');
+//   sizeSmall.addEventListener ('click', () => {
+//     let size = 16;
+//   })
+}
