5d2a97ac1211b7ba9991dcd17e4764117854f621
Trevor Hilz <trevorhilz@gmail.com>
2023-08-30 14:40:29 -0500

    Functionally works, but click handler is very slow

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const container = document.getElementById('container');
+let size;
+let numSize;
+
+sizeSelected();
+function sizeSelected () {
+  buttons = document.querySelectorAll('button');
+    buttons.forEach (button => {
+      button.addEventListener ('click', () => {
+        size = (button.id);
+        numSize = parseInt(size);
+        makeGrid();
+        // console.log(numSize);
+        // console.log(typeof(numSize));
+        })
+      })
+  }
+
 
 function makeGrid() {
-  for(let i = 0; i < 16; i++) {
+  for(let i = 0; i < numSize; i++) {
     let row = document.createElement('grid-item');
-    for(let j = 0; j < 16; j++) {
+    for(let j = 0; j < numSize; j++) {
       let cell = document.createElement('grid-item');
-      cell.style.width = "25px";
-      cell.style.height = "25px";
+      cell.style.minWidth = "25px";
+      cell.style.minHeight = "25px";
       cell.style.background = "yellow";
       cell.style.display = "inline-block";
       cell.innerText = (".");
-      // cell.style.border = ".5px solid #000000";
+      cell.style.border = ".5px solid #000000";
       row.appendChild(cell);
       hoverColor();
     }
     document.getElementById('container').appendChild(row);
   }
 }
-
-function theGrid () {
-  grid-template-column = 
-}
-
-makeGrid();
 
 function hoverColor (){
   let items = document.querySelectorAll('grid-item');
@@ -31,30 +42,4 @@ function hoverColor (){
       item.style.backgroundColor = 'black';
     })
   })
-}
-
-sizeSelected();
-
-
-function sizeSelected () {
-  buttons = document.querySelectorAll('button');
-  let size;
-    buttons.forEach (button => {
-      button.addEventListener ('click', () => {
-        if (small) {
-          size = 16;
-        } else if (medium) {
-          size = 32;
-        } else if (large) {
-          size = 64;
-          
-        }
-        console.log(size);
-      })
-    })
-
-//   let sizeSmall = documentSelector('small');
-//   sizeSmall.addEventListener ('click', () => {
-//     let size = 16;
-//   })
 }
